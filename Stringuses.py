0952f8ae66ce9a91dbf5fbf1d798caa1d44b194e
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:10:35 +0500

    Stringuses.py

---
+++ Stringuses.py
@@ -0,0 +1,26 @@
+
+
+
+
+a='My name is Masroor Ali'
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.count("is"))
+print(a.find("is"))
+print(a.index("is"))
+print(a.startswith("My"))
+print(a.endswith("masroor"))
+print(a.center(50))
+print(a.split(" "))
+print(a.rstrip("masroor"))
+print(a.islower())
+print(a.isupper())
+print(a.isprintable())
+print(a.title())
+print(a.istitle())
+print(a.swapcase())
+print(a.isspace())
+print(a.isalnum())
+print(a.isalpha())
+print(a.capitalize())
