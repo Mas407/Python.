291ac9a8ac039e178ebb172114c79cbd60a25b08
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:06:08 +0500

    Adition.py

---
+++ Even/ODD/IF_else/Adition.py
@@ -0,0 +1,20 @@
+
+a=int(input("Enter the number "))
+if(a%2==0):
+    print("Number is Even ",a%2)
+
+else:
+    print("Number is odd ",a%2)
+   
+
+a=int(input("Enter the number "))
+b=int(input("Enter the number "))
+
+if(a+b>10 and a+b<20):
+    print("You have ....",a+b,"Rupees")
+elif(a+b>=20):
+    print("You have ...",a+b,"RupeesL")
+else:
+    print("INVLID")
+
+
