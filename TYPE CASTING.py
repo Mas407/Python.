c2df566308ec16d48c524bcadef3e3c575b69a84
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:14:31 +0500

    TYPE CASTING.py

---
+++ TYPE CASTING.py
@@ -0,0 +1,21 @@
+
+
+
+inplicit type casting 
+
+is ma kuch is tarha hota ha ka ham na number ko initilizate karwa lia oor ham na ad karwaya to wo automatically float data tpye ban gaye q ka values ham na us hisab sa de thi
+is lia is tarha hoya python ma ya kam khud ho jaata ha means value sa compiler pata laga lata ha ka konsa data type use karna ha 
+
+a=2;
+b=4.3;
+print(a+b)
+
+
+Explicit type casting 
+
+a="2"
+b="3"
+#ya dono values ham na string ma de han to is ma hama manual change karna ho ga to ham is tarha sa kara ga
+
+print(int(a)+int(b))  yaha pa ham na manual change kar dia ka bhi ja ka dakho string ma number ha agr ha to ad kkar do isko
+
