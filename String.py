ae3625aa9797225ed7bebe88ecc7ec97558e2962
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:21:48 +0500

    String.py

---
+++ String.py
@@ -0,0 +1,36 @@
+
+a="Masroor"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+# soo on...
+
+#              secound step
+
+a="Masroor"
+print(a[0:6])  #yaha pa kuch is tarha ha ka ham na index dia ha 0 sa 6 tak to 7 words print hona chayia 
+#but yaha pa jo 0 ha interpreter isko read kara gaa but last 6 wala index pa jo digit ya character hoya ignore kar da ga
+
+
+a='Masroor'
+print(a[0:7])
+print(len(a))
+
+
+# means lenght us variable ke jo bi ho means jasa 7 ha lenght a variable ke to shoro 0 sa ho raha ha agla index -4 ha
+#  to len-4 ho jaya ga automaticaaly ya phr ap khud bi laga sakta han to peacha bacha 3 phr 0 sa 3 tak print ho ga
+# 3 ignore ho jaya ga 0 sa 2 tak Mas print ho jaya ga 
+
+
+
+a="Masroor"
+print(a[-4:-2])
+
+
+a="Masroor"
+print(len(a))
+
+
+#Strings are immutable means ka inko change nhi kia ja sakta insa sa new bani ja sakti ha jasa ma apko neacha bana ka dakta ho
+
