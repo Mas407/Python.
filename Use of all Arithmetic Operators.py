7cee4b6d149344ed32950ea581df8384cb5d089a
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:19:39 +0500

    Use of all Arithmetic Operators.py

---
+++ Use of all Arithmetic Operators.py
@@ -0,0 +1,30 @@
+
+comments
+
+# this special word is use for single line comment
+
+'''
+
+
+'''
+
+this three up and down single cot is use for multi line comments
+
+
+
+
+
+a=input("Enter the first number ")
+b=input("Enter the secound number ")
+print("Sum of the number is ",int(a)+int(b))
+
+a=int(input("Enter the number "))
+b=int(input("Enter the secound number "))
+
+print("Sum of the number ",a+b)
+print("Substraction of the number ",a-b)
+print("Division of the number ",a/b)
+print("floor division of the number ",a//b)
+print("Modulas of the number ",a%b)
+print("Multiplication of the number Expotentional operator ",a**b)
+
