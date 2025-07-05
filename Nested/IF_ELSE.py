36c771f10f0feef97ee6098e985ad9904a41d20c
Masroor Ali <masroorali0729@gmail.com>
2025-07-06 00:07:30 +0500

    NestedIF_ELSE.py

---
+++ Nested/IF_ELSE.py
@@ -0,0 +1,19 @@
+
+
+
+a=int(input("Enter the number"))
+b=int(input("Enter the number"))
+c=int(input("Enter the number"))
+
+if(a>b):
+    if(a>c):
+        print("A is the largest number ")
+    else:
+        print("C is the largest number ")
+else:
+    if(b>c):
+        print("B is the largest number ")
+    else:
+        print("C is the largest number")
+   
+   
